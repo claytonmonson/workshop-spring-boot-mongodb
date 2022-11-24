3d174331518ee29c2848244814b9c4d417ceb294
Clayton <clayton_monson@hotmail.com>
2022-11-24 08:46:50 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/claytoncorp/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.claytoncorp.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.claytoncorp.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
